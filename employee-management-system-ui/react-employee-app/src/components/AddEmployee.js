24dc96098f2e53ca0947d4db5bff1d580bb74a80
JesusMoralesCa <jesus.morales.carr@gmail.com>
2023-11-30 16:20:19 +0100

    Add save Employee UI

--- employee-management-system-ui/react-employee-app/src/components/AddEmployee.js
+++ employee-management-system-ui/react-employee-app/src/components/AddEmployee.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import EmployeeService from '../services/EmployeeService';
 
 const AddEmployee = () => {
 
@@ -13,6 +14,15 @@ const AddEmployee = () => {
         const value = e.target.value;
         setEmployee({...employee,[e.target.name]: value});
     }
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+        EmployeeService.saveEmployee(employee).then((response) => {
+            console.log(response);
+        }).catch((error) =>{
+            console.log(error);
+        })
+    };
 
   return (
     <div className="flex max-w-2xl">
@@ -47,7 +57,8 @@ const AddEmployee = () => {
             </div>
 
             <div className="items-center justify-center h-14 w-full my-4 space-x-4 pt-4">
-                <button className="rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700">Save</button>
+                <button onClick={saveEmployee}
+                        className="rounded text-white font-semibold bg-green-400 py-2 px-6 hover:bg-green-700">Save</button>
                 <button className="rounded text-white font-semibold bg-red-400 py-2 px-6 hover:bg-red-700">Clear</button>
             </div>
 
