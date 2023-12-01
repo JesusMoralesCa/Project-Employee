a319f5f462c0eec2398ddeac06bdef944991e9ad
JesusMoralesCa <jesus.morales.carr@gmail.com>
2023-12-01 14:27:34 +0100

    Add EmployeesList

--- employee-management-system-ui/react-employee-app/src/components/EmployeeList.js
+++ employee-management-system-ui/react-employee-app/src/components/EmployeeList.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import { useNavigate } from 'react-router-dom'
+import EmployeeService from '../services/EmployeeService';
+import Employee from "./Employee";
 
 const EmployeeList = () => {
 
-    useNavigate
+    const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(true);
+    const [employees, setEmployees] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await EmployeeService.getEmployees();
+                setEmployees(response.data);
+            } catch (error) {
+                console.log(error);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    }, [])
+    
 
 
   return (
     <>
     <div className="container mx-auto my-8">
         <div className="h-12">
-            <button onClick={} className="rounded bg-slate-600 text-white px-6 py-2 font-semibold">
+            <button onClick={() => navigate("/addEmployee")} className="rounded bg-slate-600 text-white px-6 py-2 font-semibold">
                 Add Employee
             </button>
         </div>
@@ -24,24 +44,13 @@ const EmployeeList = () => {
                         <th className="text-left font-medium text-gray-500 uppercase tracking-wider py-3 px-6">Actions</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <td className="text-left px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-gray-500">JEsus</div>
-                        </td>
-                        <td className="text-left px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-gray-500">Morales</div>
-                        </td>
-                        <td className="text-left px-6 py-4 whitespace-nowrap">
-                            <div className="text-sm text-gray-500">@</div>
-                        </td>
-                        <td className="text-right px-6 py-4 whitespace-nowrap font-medium">
-                            <a href="#" className="text-indigo-600 hover:text-indigo-800 px-4">Edit</a>
-                            <a href="#" className="text-indigo-600 hover:text-indigo-800">Delete</a>
-
-                        </td>
-                    </tr>
+                {!loading && (
+                <tbody className="bg-white">
+                    {employees.map((employee) => (
+                        <Employee employee={employee} key={employee.id}></Employee>
+                    ))}
                 </tbody>
+                )}
             </table>
 
         </div>
